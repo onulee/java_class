8bc548b9f6e3009d4268e6d00c4a73c066e49a06
D-T <D-T@DESKTOP-PS5316N>
2022-09-26 12:06:45 +0900

    commit

--- p0926/src/p0926/J0926_05.java
+++ p0926/src/p0926/J0926_05.java
@@ -1,5 +1,7 @@
 package p0926;
 
+import java.util.Scanner;
+
 public class J0926_05 {
 
 	public static void main(String[] args) {
@@ -8,6 +10,27 @@ public class J0926_05 {
 		// -------------------------
 		// 100   100  99   299   소수점2자리까지출력
 		// -------------------------
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("국어점수 입력 : ");
+		String input1 = scan.nextLine();
+		int num1 = Integer.parseInt(input1);
+		
+		System.out.println("영어점수 입력 : ");
+		String input2 = scan.nextLine();
+		int num2 = Integer.parseInt(input2);
+		
+		System.out.println("수학점수 입력 : ");
+		String input3 = scan.nextLine();
+		int num3 = Integer.parseInt(input3);
+		// 총점 total - test
+		int total = num1+num2+num3;
+		double avg = (num1+num2+num3)/3.0;
+		System.out.println("---------------------------------------");
+		System.out.print("국어\t영어\t수학\t총점\t평균\n");
+		System.out.println("---------------------------------------");
+		System.out.printf("%d\t%d\t%d\t%d\t%.2f\n",num1,num2,num3,total,avg);
 		
 
 	}

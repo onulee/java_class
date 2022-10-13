cfacfaf82579f8bacd0bba3e2ff2afa0122f92e7
D-T <D-T@DESKTOP-PS5316N>
2022-10-13 09:45:18 +0900

    숫자맞추기

--- p1012/src/p1012/J1012_06.java
+++ p1012/src/p1012/J1012_06.java
@@ -6,15 +6,20 @@ public class J1012_06 {
 
 	public static void main(String[] args) {
 		// 랜덤숫자 1-100까지 숫자를 1개 출력하시오.
+		int count=0;
 		int random = (int)(Math.random()*100)+1;
-		int[] num = new int[100];
+		int[] num = new int[10];
 		Scanner scan = new Scanner(System.in);
 		int choice=0;
+		
 		while(true) {
-			System.out.println("랜덤숫자를 맞추는 게임입니다. 숫자를 입력하세요.>>");
+			count++;
+			System.out.println("랜덤정답 : "+random);
+			System.out.println("랜덤숫자를 맞추는 게임입니다. "+count+"번째 숫자를 입력하세요.>>");
 			choice = scan.nextInt();
 			
-			// 입력한 숫자를 배열에 저장을 해야 합니다.
+			// 입력한 숫자를 배열에 저장.
+			num[count-1] = choice;
 			
 			if(choice==random) {
 				System.out.println("정답입니다.");
@@ -24,9 +29,26 @@ public class J1012_06 {
 			}else {
 				System.out.println("입력한 숫자보다 큽니다. 다시 도전하세요.!!");
 			}
+			// 10번까지 도전
+			if (count>=10) {
+				break;
+			}
+			
 		}//while
 		
-		//for 배열을 출력해야 합니다.
+		System.out.println("정답 맞추기 게임이 끝났습니다.!!");
+	    System.out.println("당신이 입력한 숫자 : ");
+	    for(int i=0;i<count;i++) {
+	    	if(i==0) {
+	    		System.out.print(num[i]);
+	    	}else {
+	    		System.out.print(","+num[i]);
+	    	}
+	    }
+	    System.out.println();
+	    System.out.println("정답숫자 : "+random);
+		
+		
 
 	}
 
---
+++ p1013/src/p1013/J1013_01.java
@@ -0,0 +1,10 @@
+package p1013;
+
+public class J1013_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
